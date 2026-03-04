d290b0eeb9468f573967bc8121eb28e2bbf3d43b
Shrish Kerur <theshrish46@gmail.com>
2026-03-04 13:19:11 +0530

    added some python code

---
+++ add.py
@@ -0,0 +1,11 @@
+def addition(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 + num2
+
+def multiply(num1, num2):
+    return num + num2
+
+def divide(num1, num2):
+    return num1 / num12
